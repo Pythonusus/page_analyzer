7c79b30c9aac0f4674bdb0d60a831a2f10b6fbc7
Petr Malafeev <StandBy545@yandex.ru>
2024-07-26 02:12:44 +0300

    Fix logo load issue

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template
 
 app = Flask(__name__)
 
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# Обработчик загрузки статики
+@app.route("/<file>")
+def get_static(file):
+    with open(f"page_analyzer/static/{file}", mode="rb") as f:
+        logo = f.read()
+    res = make_response(logo)
+    res.headers["Content-Type"] = "image/png"
+    return res
