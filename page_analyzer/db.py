6b1b43bf5d213936403634e1401db07fa8a21e52
Petr Malafeev <StandBy545@yandex.ru>
2024-08-15 00:31:55 +0300

    Upd db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -6,3 +6,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def add_url_to_db(url):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO urls (name, created_at)
+                VALUES (%(str)s, %(date)s)
+                RETURNING id;
+                """,
+                {"name": url, "date": dt.now().isoformat()},
+            )
+            url_id = cur.fetchone()["id"]
+    conn.close()
+    return url_id
