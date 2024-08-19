b18eca15292e6654d969fa83d659ee90748b7777
Petr Malafeev <StandBy545@yandex.ru>
2024-08-20 00:05:02 +0300

    Upd add_url_to_db func
    Now it uses NamedTupleCursor
    
    created_at is now date without time to comply with hexlet
    technical specifications

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,7 +1,7 @@
 import os
-from datetime import datetime as dt
+from datetime import date as dt
 
-import psycopg2
+from psycopg2 import connect, extras
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,16 +9,16 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def add_url_to_db(url):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
+    with connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
             cur.execute(
                 """
                 INSERT INTO urls (name, created_at)
                 VALUES (%(str)s, %(date)s)
                 RETURNING id;
                 """,
-                {"name": url, "date": dt.now().isoformat()},
+                {"name": url, "date": dt.today().isoformat()},
             )
-            url_id = cur.fetchone()["id"]
+            url_id = cur.fetchone().id
     conn.close()
     return url_id
