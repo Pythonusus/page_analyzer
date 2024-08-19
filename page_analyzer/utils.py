3bb810a1c7232bb693208eadba16c062ef8b3ef6
Petr Malafeev <StandBy545@yandex.ru>
2024-08-19 23:54:52 +0300

    Add url validation util

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,3 +1,16 @@
 from urllib.parse import urlparse, urlunparse
+
+from validators.url import url as url_validator
+
+MAX_URL_LENGTH = 255
+
+
 def normalize_url(url):
     return urlunparse(urlparse(url))
+
+
+def validate_url(url):
+    if not url_validator(url):
+        return "Некорректный URL"
+    elif len(url) > MAX_URL_LENGTH:
+        return "URL превышает 255 символов"
