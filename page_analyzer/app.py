00c6596ffcdd2a266eb21cbefaeb5943feff5a67
Petr Malafeev <StandBy545@yandex.ru>
2024-08-19 23:08:43 +0300

    Add SECRET_KEY to app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,6 +9,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
 # Обработчик главной страницы
