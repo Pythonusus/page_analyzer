f2b4b629fabdca2ae6eccdbb10d48c520e8d96f9
Petr Malafeev <StandBy545@yandex.ru>
2024-08-24 17:45:48 +0300

    Add url_checks table

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE NOT NULL
+);
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE NOT NULL,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
     created_at DATE NOT NULL
 );
