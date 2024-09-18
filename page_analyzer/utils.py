8f6f500027120728ca54f562120769459d6634bd
Petr Malafeev <StandBy545@yandex.ru>
2024-09-18 23:04:01 +0300

    Add parse_html utility

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse
 
+from bs4 import BeautifulSoup
 from validators import url as url_validator
 
 MAX_URL_LENGTH = 255
@@ -15,3 +16,22 @@ def validate_url(url):
         return "Некорректный URL"
     if len(url) > MAX_URL_LENGTH:
         return "URL превышает 255 символов"
+
+
+def parse_html(html):
+    """
+    Parse h1, title, description content.
+
+    Args:
+        html (str): HTML string or an open filehandler
+
+    Returns:
+        dict: keys "h1", "title", "description" with parsed data or None
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    data = {}
+    data["h1"] = soup.h1.string
+    data["title"] = soup.title.string
+    description = soup.find("meta", attrs={"name": "description"}) or {}
+    data["description"] = description.get("content")
+    return data
