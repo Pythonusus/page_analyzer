5637d9f1186d4be4c2f011ebae26989b8bbc1935
Petr Malafeev <StandBy545@yandex.ru>
2024-09-19 17:55:59 +0300

    refactor(db.py): remove code duplication

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -8,108 +8,94 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-def add_url_to_db(url):
+def exec_query(query, params=None):
     with connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
-            cur.execute(
-                """
-                INSERT INTO urls (name, created_at)
-                VALUES (%(name)s, %(date)s)
-                RETURNING id;
-                """,
-                {"name": url, "date": dt.today()},
-            )
-            url_id = cur.fetchone().id
+            cur.execute(query, params)
+            data = cur.fetchall()
     conn.close()
-    return url_id
+    return data
 
 
 def find_url_by_name(url):
-    with connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
-            cur.execute("SELECT * FROM urls WHERE name = %s;", (url,))
-            data = cur.fetchone()
-    conn.close()
-    return data
+    query_result = exec_query("SELECT * FROM urls WHERE name = %s;", (url,))
+    return query_result[0] if query_result else None
 
 
 def find_url_by_id(url_id):
-    with connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
-            cur.execute("SELECT * FROM urls WHERE id = %s;", (url_id,))
-            data = cur.fetchone()
-    conn.close()
-    return data
+    query_result = exec_query("SELECT * FROM urls WHERE id = %s;", (url_id,))
+    return query_result[0] if query_result else None
 
 
 def get_all_urls():
-    with connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
-            cur.execute(
-                """
-                WITH last_checks AS (
-                SELECT *
-                FROM url_checks AS checks1
-                WHERE checks1.id = (
-                    SELECT MAX(checks2.id)
-                    FROM url_checks AS checks2
-                    WHERE checks1.url_id = checks2.url_id
-                )
-                )
-
-                SELECT
-                    urls.id,
-                    urls.name,
-                    last_checks.status_code,
-                    last_checks.created_at AS last_check
-                FROM urls
-                LEFT JOIN last_checks ON urls.id = last_checks.url_id
-                ORDER BY urls.id DESC;
-                """
+    query = """
+        WITH last_checks AS (
+        SELECT *
+        FROM url_checks AS checks1
+        WHERE checks1.id = (
+            SELECT MAX(checks2.id)
+            FROM url_checks AS checks2
+            WHERE checks1.url_id = checks2.url_id
             )
-            data = cur.fetchall()
-    conn.close()
-    return data
+        )
 
-
-def add_check_to_db(url_id, status_code, data):
-    with connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
-            cur.execute(
-                """
-                INSERT INTO url_checks
-                (url_id, status_code, h1, title, description, created_at)
-                VALUES (
-                    %(url_id)s,
-                    %(status_code)s,
-                    %(h1)s,
-                    %(title)s,
-                    %(description)s,
-                    %(created_at)s
-                )
-                RETURNING id;
-                """,
-                {
-                    "url_id": url_id,
-                    "status_code": status_code,
-                    "h1": data.get("h1"),
-                    "title": data.get("title"),
-                    "description": data.get("description"),
-                    "created_at": dt.today()
-                }
-            )
-            check_id = cur.fetchone().id
-    conn.close()
-    return check_id
+        SELECT
+            urls.id,
+            urls.name,
+            last_checks.status_code,
+            last_checks.created_at AS last_check
+        FROM urls
+        LEFT JOIN last_checks ON urls.id = last_checks.url_id
+        ORDER BY urls.id DESC;
+    """
+    query_result = exec_query(query)
+    return query_result
 
 
 def get_all_url_checks(url_id):
-    with connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=extras.NamedTupleCursor) as cur:
-            cur.execute(
-                "SELECT * FROM url_checks WHERE url_id = %s;",
-                (url_id,)
-            )
-            data = cur.fetchall()
-    conn.close()
-    return data
+    query_result = exec_query(
+        "SELECT * FROM url_checks WHERE url_id = %s;",
+        (url_id,)
+    )
+    return query_result
+
+
+def add_url_to_db(url):
+    query = """
+        INSERT INTO urls (name, created_at)
+        VALUES (%(name)s, %(date)s)
+        RETURNING id;
+    """
+
+    query_result = exec_query(query, {"name": url, "date": dt.today()})
+    url_id = query_result[0].id
+    return url_id
+
+
+def add_check_to_db(url_id, status_code, data):
+    query = """
+        INSERT INTO url_checks
+        (url_id, status_code, h1, title, description, created_at)
+        VALUES (
+            %(url_id)s,
+            %(status_code)s,
+            %(h1)s,
+            %(title)s,
+            %(description)s,
+            %(created_at)s
+        )
+        RETURNING id;
+    """
+
+    params = {
+        "url_id": url_id,
+        "status_code": status_code,
+        "h1": data.get("h1"),
+        "title": data.get("title"),
+        "description": data.get("description"),
+        "created_at": dt.today()
+    }
+
+    query_result = exec_query(query, params)
+    check_id = query_result[0].id
+    return check_id
