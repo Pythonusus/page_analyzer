a890e6c51a59d42c3d7a128f7eadbe149ae09c8e
Petr Malafeev <StandBy545@yandex.ru>
2024-09-21 12:44:07 +0300

    add: DATABASE_URL to app.config

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -30,6 +30,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 
 
 @app.route("/")
